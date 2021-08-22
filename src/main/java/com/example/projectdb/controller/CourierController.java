630a99a156e6b1cb4423ce62833e100585b4e9f9
FengWan <e0696870@u.nus.edu>
2021-08-22 13:33:34 +0800

    addFoodBtn

--- src/main/java/com/example/projectdb/controller/CourierController.java
+++ src/main/java/com/example/projectdb/controller/CourierController.java
@@ -2,16 +2,20 @@ package com.example.projectdb.controller;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.projectdb.model.FoodItem;
+import com.example.projectdb.repo.FoodItemRepository;
 import com.example.projectdb.repo.HawkerListingRepository;
+import java.util.List;
 
 @RestController
 
@@ -19,6 +23,9 @@ public class CourierController {
 	
 	@Autowired
 	HawkerListingRepository hrepo;
+	
+	@Autowired
+	FoodItemRepository frepo;
 	
 	
 	@RequestMapping("/hawkerlisting/All")
@@ -38,6 +45,21 @@ public class CourierController {
 		ArrayList<ArrayList<String>> food = (ArrayList<ArrayList<String>>)hrepo.findFoodItemByHawkerId(HawkerId);
 		
 		return new ResponseEntity<ArrayList<ArrayList<String>>>(food,HttpStatus.OK);
+	}
+	
+	@RequestMapping("/courier/food")
+	public ResponseEntity<String> createFood(@RequestBody FoodItem newFoodItem){
+		
+		List<FoodItem> foodItems = frepo.findAll();
+		
+		for(FoodItem f:foodItems) {
+			if(f.getName()==newFoodItem.getName()) {
+				return new ResponseEntity<String>("FAILED",HttpStatus.BAD_REQUEST);
+			}
+		}
+		
+		frepo.saveAndFlush(newFoodItem);
+		return new ResponseEntity<String>("SUCCESS", HttpStatus.CREATED);
 	}
 
 
---
+++ src/main/java/com/example/projectdb/repo/FoodItemRepository.java
@@ -0,0 +1,16 @@
+package com.example.projectdb.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.projectdb.model.FoodItem;
+
+public interface FoodItemRepository extends JpaRepository<FoodItem, Long> {
+	
+//	@Modifying
+//	@Query(value="INSERT INTO food_item (name) VALUES (:name)", nativeQuery=true)
+//	public void saveFood(@Param("name") String name);
+
+}
